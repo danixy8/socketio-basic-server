1c141b4607f7dda52a760b8c1d63a77bb643a425
Daniel <danielguve2@gmail.com>
2021-06-13 21:25:20 -0400

    se agrego CORS

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors/');
 
 class Server {
 
@@ -23,6 +24,9 @@ class Server {
     middlewares(){
         //desplegar el directorio publico
         this.app.use( express.static(path.resolve(__dirname, '../public') ) );
+
+        //CORS
+        this.app.use( cors() );
     }
 
     socketsSettings(){
